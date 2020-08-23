acc31582165e5ba9af8f1bd7e0ce2a9185838ee6
nitinkumar28127 <emailto28127@gmail.com>
2020-08-23 12:22:43 +0530

    Added Files

--- Pages/homePage.py
+++ Pages/homePage.py
@@ -1,15 +1,16 @@
+from Locators.locators import Locators
+
+
 class HomePage:
 
     def __init__(self, driver):
         self.driver = driver
 
-        self.welcome_link_id = "welcome"
-        self.logout_link_linkText = "Logout"
-        self.login_button_id = "btnLogin"
+        self.welcome_link_id = Locators.welcome_link_id
+        self.logout_link_linkText = Locators.logout_link_linkText
 
     def click_welcome(self):
-        self.driver.find_element_by_id("welcome").click()
+        self.driver.find_element_by_id(self.welcome_link_id).click()
 
     def click_logout(self):
-        self.driver.find_element_by_link_text("Logout").click()
-
+        self.driver.find_element_by_link_text(self.logout_link_linkText).click()
--- Test/Login.py
+++ Test/Login.py
@@ -2,19 +2,27 @@ from selenium import webdriver
 import time
 import unittest
 
+from Pages.homePage import HomePage
+from Pages.loginPage import LoginPage
+
 
 class LoginTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome("..\\drivers\\chromedriver.exe")
-        cls.driver.implicitly_wait(10)
+        cls.driver.implicitly_wait(20)
         cls.driver.maximize_window()
 
     def test_login_valid(self):
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
-
-
+        login= LoginPage(self.driver)
+        login.enter_username("Admin")
+        login.enter_password("admin123")
+        login.click_login()
+        home=HomePage(self.driver)
+        home.click_welcome()
+        home.click_logout()
         time.sleep(2)
 
     @classmethod
